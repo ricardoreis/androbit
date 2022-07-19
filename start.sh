9ad36d0140fd6a12a3305ea67a571f2837298688
Ricardo Reis <ricardo.luis.reis@gmail.com>
2022-07-19 19:19:14 +0000

    Atualizando start.sh e stop.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 BLOCKCHAIN_FOLDER=$(find /storage -type d -name "com.termux" 2>/dev/null)/files/blockchain
-echo $BLOCKCHAIN_FOLDER
+#echo $BLOCKCHAIN_FOLDER
 
 if [ -f /root/bitcoin-core/bin/bitcoind ]; then
     /root/bitcoin-core/bin/bitcoind -conf=$BLOCKCHAIN_FOLDER/bitcoin.conf -datadir=$BLOCKCHAIN_FOLDER -daemon
--- stop.sh
+++ stop.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 BLOCKCHAIN_FOLDER=$(find /storage -type d -name "com.termux" 2>/dev/null)/files/blockchain
-echo $BLOCKCHAIN_FOLDER
+#echo $BLOCKCHAIN_FOLDER
 
 if [ -f $BLOCKCHAIN_FOLDER/bitcoind.pid ]; then
     kill $(cat $BLOCKCHAIN_FOLDER/bitcoind.pid)
